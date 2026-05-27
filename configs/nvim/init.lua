3cb67dc792a1a594e4f2ff6d87ddc8f88ad3a767
gtullio12 <gtullio12@protonmail.com>
2026-05-27 08:10:00 -0500

    update fuzzy find files

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -60,7 +60,7 @@ require("lazy").setup({
 
       local builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<S-p>", builtin.find_files)
       vim.keymap.set("n", "<leader>fg", builtin.live_grep)
       vim.keymap.set("n", "<leader>fb", builtin.buffers)
 
